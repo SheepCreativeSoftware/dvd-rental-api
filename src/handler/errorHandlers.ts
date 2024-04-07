dbb92945e5b33806712b8dd2266a5808478fde1c
Magraina <33145691+magraina@users.noreply.github.com>
2024-04-07 22:17:59 +0200

    feat: improved error handling

--- src/handler/errorHandlers.ts
+++ src/handler/errorHandlers.ts
@@ -2,34 +2,40 @@
 import type { NextFunction, Request, Response } from 'express';
 import { buntstift } from 'buntstift';
 
+const logOnError = (err: unknown, _req: Request, _res: Response, next: NextFunction) => {
+	if (err instanceof Error) {
+		buntstift.error(`${err.name}: ${err.message}`);
+		if (err.stack) {
+			buntstift.error(err.stack);
+		}
+		if (err.cause) {
+			buntstift.error(JSON.stringify(err.cause));
+		}
+	}
+	next(err);
+};
+
 // Handle unknown routes error
 const notFoundHandler = (_req: Request, res: Response) => {
-	res.status(404).send('Not Found');
+	res.status(404).send({ message: 'Not Found' });
 };
 
 // Handle Errors in middlewares
-const clientErrorHandler = (err: Error, _req: Request, res: Response, next: NextFunction) => {
+const clientErrorHandler = (err: unknown, _req: Request, res: Response, next: NextFunction) => {
 	if (err instanceof Error) {
-		buntstift.error(`${err.name}: ${err.message}`);
-	}
-	if (err.stack) {
-		buntstift.error(err.stack);
-	}
-	if (err.name === 'Forbidden') {
-		return res.status(403).send('Forbidden');
+		switch (err.name) {
+			case 'Bad Request':
+				return res.status(400).send({ message: 'Bad Request' });
+			case 'Forbidden':
+				return res.status(403).send({ message: 'Forbidden' });
+		}
 	}
 	return next(err);
 };
 
 // Handle unexpected errors
-const errorHandler = (err: Error, _req: Request, res: Response) => {
-	if (err instanceof Error) {
-		buntstift.error(`${err.name}: ${err.message}`);
-	}
-	if (err.stack) {
-		buntstift.error(err.stack);
-	}
-	res.status(400).send('Bad Request');
+const errorHandler = (_err: unknown, _req: Request, res: Response, _next: NextFunction) => {
+	res.status(500).send({ message: 'Internal Server Error' });
 };
 
-export { errorHandler, clientErrorHandler, notFoundHandler };
+export { errorHandler, clientErrorHandler, logOnError, notFoundHandler };
